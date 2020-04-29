81a728170d701101f0ccec3c9856ffe9e5f38772
SPootSP <64023183+SPootSP@users.noreply.github.com>
2020-04-29 10:37:11 +0200

    Update upload_function.js

--- js/upload_function.js
+++ js/upload_function.js
@@ -1,18 +1,22 @@
-function handleFileSelect(evt) {
+function openfileDrop(evt) {
     evt.stopPropagation();
     evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
-
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
+    var input = evt.dataTransfer; // FileList object.
+    if (input.files[0].type != "application/vnd.ms-excel"){
+      alert("wrong file type. We require a .csv file not a " + input.files[0].type + " file");
+    } else {
+      var reader = new FileReader();
+      var aftertext = "";
+      reader.onload = function(){
+        aftertext = reader.result;
+        storetext(aftertext);
+      };
+      reader.onloadend = function(){
+        console.log("done loading");
+      };
+      reader.readAsText(input.files[0]);
     }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
   }
 
   function handleDragOver(evt) {
@@ -24,4 +28,26 @@ function handleFileSelect(evt) {
   // Setup the dnd listeners.
   var dropZone = document.getElementById('drop_zone');
   dropZone.addEventListener('dragover', handleDragOver, false);
-  dropZone.addEventListener('drop', handleFileSelect, false);
+  dropZone.addEventListener('drop', openfileDrop, false);
+
+  var openfileBrowse = function(event){
+    var input = event.target;
+    if (input.files[0].type != "application/vnd.ms-excel"){
+      alert("wrong file type. We require a .csv file not a " + input.files[0].type + " file");
+    } else {
+      var reader = new FileReader();
+      var aftertext = "";
+      reader.onload = function(){
+        aftertext = reader.result;
+        storetext(aftertext);
+      };
+      reader.onloadend = function(){
+        console.log("done loading");
+      };
+      reader.readAsText(input.files[0]);
+    }
+  };
+
+  function storetext(text) {
+    console.log(text.substring(0,200));
+  }
