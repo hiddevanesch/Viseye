7b92770bd512d31f5f654b04452cc470bd3dbdde
hiddevanesch <64023154+hiddevanesch@users.noreply.github.com>
2020-05-01 12:16:14 +0200

    Another MAJOR file system update
    Updated the file system generated by Vue CLI

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p class="p bmar-small tx-dgray">Welcome to Viseye, a tool that visualizes eye movement data. The tool has been created for a DBL project by TU/e students.</p>
-    <p class="p bmar-small tx-dgray">To start the visualization, please upload a .csv file in the correct format.</p>
+    <p class="p bmar-small tx-dgray">To start the visualization with your own data, please head to the upload page by pressing the button below.</p>
+    <button></button>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from './router';
+import store from './store';
 
-var app = new Vue({
-  el: '#app'
-})
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
+
