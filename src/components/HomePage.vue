1d8bf4753122a86fd6d2a56bcec0ab1c7961a482
hiddevanesch <64023154+hiddevanesch@users.noreply.github.com>
2020-05-01 10:58:24 +0200

    MAJOR file system update.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p class="p bmar-small tx-dgray">Welcome to Viseye, a tool that visualizes eye movement data. The tool has been created for a DBL project by TU/e students.</p>
+    <p class="p bmar-small tx-dgray">To start the visualization, please upload a .csv file in the correct format.</p>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+var app = new Vue({
+  el: '#app'
+})
