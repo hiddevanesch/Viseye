ca0669c78dc2337aa93fb477b27165ee49d5a1ba
hiddevanesch <64023154+hiddevanesch@users.noreply.github.com>
2020-05-02 16:21:30 +0200

    Linked firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from './router';
 import store from './store';
+import * from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgDQ4YNTxPICT0IV6cKqg-qStmsG0qgiY",
+  authDomain: "viseye-57f4e.firebaseapp.com",
+  databaseURL: "https://viseye-57f4e.firebaseio.com",
+  projectId: "viseye-57f4e",
+  storageBucket: "viseye-57f4e.appspot.com",
+  messagingSenderId: "780525324899",
+  appId: "1:780525324899:web:577a5386e8dbe3377ac656",
+  measurementId: "G-5PRR2E8R3P"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
