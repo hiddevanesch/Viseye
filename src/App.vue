d072ab87717297d60f2548465517d9d600769a23
hiddevanesch <64023154+hiddevanesch@users.noreply.github.com>
2020-05-03 23:08:52 +0200

    Small changes, last commit of the day (I think)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <router-view />
+  <transition name="fade" mode="in-out">
+    <router-view />
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'Viseye app',
+  data() {
+    return {
+      prevHeight: 0
+    };
+  }
 }
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+</style>
--- src/tools/upload_function.js
+++ src/tools/upload_function.js
@@ -2,6 +2,8 @@ function openfileDrop(evt) {
     evt.stopPropagation();
     evt.preventDefault();
 
+    console.log(overallData.length);
+
     var input = evt.dataTransfer; // FileList object.
     if (input.files[0].type != "application/vnd.ms-excel"){
       alert("wrong file type. We require a .csv file not a " + input.files[0].type + " file");
@@ -30,6 +32,8 @@ function openfileDrop(evt) {
   dropZone.addEventListener('dragover', handleDragOver, false);
   dropZone.addEventListener('drop', openfileDrop, false);
 
+  var overallData = [];
+
   var openfileBrowse = function(event){
     var input = event.target;
     if (input.files[0].type != "application/vnd.ms-excel"){
@@ -49,6 +53,59 @@ function openfileDrop(evt) {
   }
 
   function storetext(text) {
-    console.log(text.split(/\r\n|\n/).length);
-    console.log(text.substring(0,200));
+    var jsonData = tsvJSON(text);
+    // var nested_data = d3.nest()
+    //                   .key(function(d) { return d.StimuliName; })
+    //                   .entries(jsonData);
+    overallData = jsonData;
+    var svgContainer = d3.select("#painter").append("svg")
+                                      .attr("width", 200)
+                                      .attr("height", 200);
+
+    var circles = svgContainer.selectAll("circle")
+                              .data(dat.map(function(d){return d.FixationIndex}))
+                              .enter()
+                              .append("circle");
+
+    var circleAttributes = circles
+                          .attr("cx", function (d) { return d; })
+                          .attr("cy", function (d) { return d; })
+                          .attr("r", 20 )
+                          .style("fill", function(d) {
+                          var returnColor;
+                          if (d === 30) { returnColor = "green";
+                          } else if (d === 70) { returnColor = "purple";
+                          } else if (d === 110) { returnColor = "red"; }
+                          return returnColor;
+                          });
   }
+
+  //from http://techslides.com/convert-csv-to-json-in-javascript
+  //var tsv is the TSV file with headers
+function tsvJSON(csv){
+
+  var lines=csv.split("\n");
+
+  // NOTE: If your columns contain commas in their values, you'll need
+  // to deal with those before doing the next step
+  // (you might convert them to &&& or something, then covert them back later)
+  // jsfiddle showing the issue https://jsfiddle.net/
+  var headers=lines[0].split("	");
+
+  var result = [];
+  for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split("	");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+
+  }
+
+  //return result; //JavaScript object
+  return JSON.parse(JSON.stringify(result)); //JSON
+}
