72025352422e0319e007e4fe5b147fb2e9cb644b
JiapengFan <j.fan@student.tue.nl>
2020-05-03 12:56:36 +0200

    Scatterplto with working dropdown menu filter, background doesn't work.

--- js/csvtovis.js
+++ js/csvtovis.js
@@ -1,68 +1,187 @@
-import {
-  select,
-  csv,
-  scaleLinear,
-  axisLeft,
-  axisBottom,
-  max,
-  min
-} from 'd3';
-
-const svg = select('svg');
+const svg = d3.select('svg');
 
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
-//Function render
-const render = data => {
-	const xValue = d => d.MappedFixationPointX;
-  const yValue = d => d.MappedFixationPointY;
+let data;
+let stimulusName;
+let allVersions = [];
+let dataSelected;
 
-  const radius = 2;
-  const margin = { top: 20, right: 20, bottom: 50, left: 60 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
+//function dropdown menu
+const dropdownMenu = (selection, props) => {
+	const {
+		options,
+		onOptionClicked
+	} = props;
 
-  //Transform the data values into positions
-  const xScale = scaleLinear()
-  	.domain([min(data, xValue), max(data, xValue)])
-    .range([0, innerWidth])
-  	.nice();
+	//Create select keyword with click event listener
+	let select = selection.selectAll('select').data([null]);
+	select = select.enter().append('select')
+		.merge(select)
+			.on('change', function() {
+				onOptionClicked(this.value);
+			});
 
-  const yScale = scaleLinear()
-  	.domain([min(data, yValue), max(data, yValue)])
-    .range([innerHeight, 0])
-		.nice();
+	//Create options inside the dropdown menu with the corresponding value for the displayed text
+	const option = select.selectAll('option').data(options);
+	option.enter().append('option')
+		.merge(option)
+			.text(d => d)
+			.attr("value", d => d);
+	}
 
-  //Translating the visualisation to innerposition
-  const g = svg.append('g')
-  	.attr('transform', `translate(${margin.left},${margin.top})`);
-
-  const xAxis = axisBottom(xScale)
-    .tickFormat(xAxisTickFormat)
-    .tickSize(-innerHeight);
-
-  //Creating the axes
-  g.append('g').call(axisLeft(yScale));
-  g.append('g').call(axisBottom(xScale))
-   .attr('transform', `translate(0,${innerHeight})`);
-
-  //Draw circles for each row of the csv file
-  g.selectAll('.dot').data(data)
-  	.enter().append('circle')
-  		.attr('cx', d => xScale(xValue(d)))
-  		.attr('cy', d => yScale(yValue(d)))
-  		.attr('r', radius)
-  		.attr('fill', 'black');
+const onStimulusNameClicked = option => {
+	stimulusName = option;
+	render();
 }
 
-console.log(csv('data.csv'));
-//Rendering the data
-csv('data.csv')
-  .then(data => {
+const scatterPlot = (selection, props) => {
+	const {
+		title,
+		xValue,
+		yValue,
+		circleRadius,
+		margin,
+	} = props;
+
+	const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+	dataSelected = data.filter(d => d.StimuliName == stimulusName);
+	let imageSelected = allVersions.filter(d => d == stimulusName);
+
+	let background = 'stimuli\\' + imageSelected;
+
+	console.log(background);
+	const img = selection.selectAll('image').data([null]);
+	const imgEnter = img.enter().append('image')
+		.attr("xlink:href", background);
+	imgEnter
+		.merge(img)
+		.transition().duration(2000)
+			.attr('width', innerWidth)
+			.attr('height', innerHeight)
+			.attr('transform',
+				`translate(${margin.left},${margin.top})`
+			);
+
+	//A color scale: one color per data selection
+	const dotColor = d3.scaleOrdinal()
+		.domain(allVersions)
+		.range(d3.schemeSet2);
+
+	//Transform the data values into positions
+	const xScale = d3.scaleLinear()
+		.domain([d3.min(dataSelected, xValue), d3.max(dataSelected, xValue)])
+		.range([0, innerWidth])
+		.nice();
+
+	const yScale = d3.scaleLinear()
+		.domain([d3.min(dataSelected, yValue), d3.max(dataSelected, yValue)])
+		.range([innerHeight, 0])
+		.nice();
+
+	//Create container for scatterplot
+	const g = selection.selectAll('.container').data([null]);
+  const gEnter = g
+    .enter().append('g')
+      .attr('class', 'container');
+	//Translating the visualisation to innerposition with the updated data
+	gEnter
+		.merge(g)
+			.attr('transform',
+				`translate(${margin.left},${margin.top})`
+			);
+
+	const xAxis = d3.axisBottom(xScale)
+		.tickSize(-innerHeight)
+		.tickPadding(10);
+
+	const yAxis = d3.axisLeft(yScale)
+		.tickSize(-innerWidth)
+		.tickPadding(10);
+
+	//Creating the axes
+	const yAxisG = g.select('.y-axis');
+	const yAxisGEnter = gEnter
+		.append('g')
+			.attr('class', 'y-axis');
+	yAxisG
+		.merge(yAxisGEnter)
+			.call(yAxis)
+			.selectAll('.domain').remove();
+
+	const xAxisG = g.select('.x-axis');
+	const xAxisGEnter = gEnter
+		.append('g')
+				.attr('class', 'x-axis');
+	xAxisG
+		.merge(xAxisGEnter)
+			.attr('transform', `translate(0,${innerHeight})`)
+			.call(xAxis)
+			.selectAll('.domain').remove();
+
+	//Draw circles for each row of the selected data
+	const circles = g.merge(gEnter)
+		.selectAll('circle').data(dataSelected);
+	circles
+		.enter().append('circle')
+			.merge(circles)
+				.attr('cx', innerWidth/2)
+				.attr('cy', innerHeight/2)
+				.attr('r', 0)
+			.transition().duration(2000)
+			.delay((d, i) => i)
+				.attr('r', circleRadius)
+				.attr('cx', d => xScale(xValue(d)))
+				.attr('cy', d => yScale(yValue(d)));
+	circles
+		.exit()
+			.transition().duration(2000)
+				.attr('r', 0)
+				.attr('cx', innerWidth/2)
+				.attr('cy', innerHeight/2)
+			.remove();
+
+	g.append('text')
+		.attr('class', 'title')
+  	.attr('y', -15)
+		.text(title);
+}
+
+//Function render
+const render = () => {
+//Invoke function dropdownMenu to generate menu
+	d3.select('#menus')
+		.call(dropdownMenu, {
+			options: allVersions,
+			onOptionClicked: onStimulusNameClicked
+			}
+		);
+
+
+
+	svg.call(scatterPlot, {
+		title: 'Scatterplot: Eye tracking data per city',
+		xValue: d => d.MappedFixationPointX,
+	  yValue: d => d.MappedFixationPointY,
+	  circleRadius: 2,
+	  margin: { top: 50, right: 20, bottom: 50, left: 60 },
+	})
+}
+
+//(RE-)Render the data according to the selection by filter
+
+d3.csv('data.csv')
+  .then(loadedData => {
+		data = loadedData;
   	data.forEach(d => {
     	d.MappedFixationPointX = +d.MappedFixationPointX;
     	d.MappedFixationPointY = +d.MappedFixationPointY;
+      if (!allVersions.includes(d.StimuliName)) {
+				allVersions.push(d.StimuliName);
+    	}
     });
-  	render(data);
+  	render()
 });
