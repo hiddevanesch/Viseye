a0a587089cb631891ff9c33114f4383a0a862b2b
SPootSP <s.j.poot@student.tue.nl>
2020-05-04 14:59:33 +0200

    second try for the screenshot

--- js/toMain.js
+++ js/toMain.js
@@ -1,4 +1,5 @@
 function startTransition(){
   document.getElementById("container").style.display = "none";
   document.getElementById("upload_box").style.display = "block";
+  document.getElementById("saveButton").style.display = "block";
 }
