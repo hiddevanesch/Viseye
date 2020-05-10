70b563dbc86f98b6e16e3f7f9c4ed2ecd058ce6c
hiddevanesch <64023154+hiddevanesch@users.noreply.github.com>
2020-05-11 00:26:12 +0200

    Changed how the styling works and made the visualization page a bit better looking

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="visualization-container">
-    <header class="topbar">
-      <h1 class="tx-dgray bg-white">Viseye.</h1>
+    <header class="topbar bg-white">
+      <router-link to="/"><h1 class="tx-dgray">Viseye.</h1></router-link>
     </header>
     <div class="visrow">
       <div class="menubar viscol col20 bg-ldgray">
-        <p>placeholder</p>
+        <button type="button" class="button button-blue bmar-small full-width">scatter&shy;plot</button>
+        <button type="button" class="button button-blue bmar-small full-width">visualization 2</button>
+        <button type="button" class="button button-blue bmar-small full-width">visualization 3</button>
+        <button type="button" class="button button-blue bmar-small full-width">visualization 4</button>
       </div>
       <div class="visualizationbox viscol col80 order2 bg-dgray">
         <scatter-plot />
@@ -24,157 +27,3 @@ export default {
 }
 </script>
 
-<style>
-* {
-    margin: 0px;
-    padding: 0px;
-    overflow: hidden;
-    box-sizing: border-box;
-  }
-
-  div {
-    display: block;
-  }
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
-  img {
-    vertical-align: middle;
-    border-style: none;
-  }
-
-  .bg-white {
-    background-color: #ffffff;
-  }
-
-  .bg-lgray {
-    background-color: #f1f1f1;
-  }
-
-  .bg-ldgray {
-    background-color: #1a1a1a;
-  }
-
-  .bg-dgray {
-    background-color: #111111;
-  }
-
-  .tx-white {
-    color: #ffffff;
-  }
-
-  .tx-black {
-    color: #000000;
-  }
-
-  .tx-lgray {
-    color: #f1f1f1;
-  }
-
-  .tx-gray {
-    color: #adadad;
-  }
-
-  .tx-viseyeblue {
-    color: #498fff;
-  }
-
-  .tx-ldgray {
-    color: #1a1a1a;
-  }
-
-  .tx-dgray {
-    color: #111111;
-  }
-
-  .tmar-small {
-    margin-top: 20px;
-  }
-
-  .tmar-med {
-    margin-top: 40px;
-  }
-
-  .tmar-big {
-    margin-top: 60px;
-  }
-
-  .bmar-small {
-    margin-bottom: 20px;
-  }
-
-  .bmar-med {
-    margin-bottom: 40px;
-  }
-
-  .bmar-big {
-    margin-bottom: 60px;
-  }
-
-.center {
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-}
-
-.visualization-container {
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-.visrow {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    margin-right: -15px;
-    margin-left: -15px;
-}
-
-.viscol {
-  position: relative;
-  width: 100%;
-  min-height: 1px;
-  height: 100vh;
-  padding-right: 15px;
-  padding-left: 15px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
-
-.col20 {
-    flex: 0 0 20%;
-    max-width: 20%;
-}
-
-.col80 {
-    flex: 0 0 80%;
-    max-width: 80%;
-}
-
-.order2 {
-    -webkit-box-ordinal-group: 3;
-    -ms-flex-order: 2;
-    order: 2;
-}
-
-.topbar {
-  padding-top: 20px;
-  padding-bottom: 20px;
-  padding-left: 30px;
-  padding-right: 30px;
-}
-
-.topbar h1 {
-    font-size: 50px;
-    font-family: 'Product Sans Black';
-    font-weight: 400;
-  }
-</style>
